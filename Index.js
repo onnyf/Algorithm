d16c0078777d4c946d43557aaa808205cdfe8a8e
onnyf <onnyf2003@gmail.com>
2024-12-09 12:05:30 -0800

    first commit

---
+++ Index.js
@@ -0,0 +1,26 @@
+// adding three numbers
+var num1 =17
+var num2 =25
+var num3 =12
+var sum = num1 + num2 + num3 
+console.log(sum)
+
+
+
+// find percentage profit
+var cp =12500
+var sp =72750
+var p =60250
+
+// selection processing
+
+var age = 25
+if (age > 17){
+    console.log ("you can vote")
+}else{
+    console.log ("you can't vote")
+}
+    
+
+
+
